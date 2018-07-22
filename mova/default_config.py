f692bf1889bb44228b39d426d0a6677759525886
Joshy Cyriac <j.cyriac@gmail.com>
2018-07-22 09:51:18 +0200

    more default config params

--- mova/default_config.py
+++ mova/default_config.py
@@ -1,8 +1,17 @@
+DEBUG=False
+
+## Location of where the image data should be donwloaded to (Full path!)
+IMAGE_FOLDER = '/home/foo/image_data'
+
 # DCMTK settings
 DCMIN = '/Applications/dcmtk/dcm.in'
 DCMTK_BIN = '/Applications/dcmtk/dcmtk-3.6.0-mac-i686-dynamic/bin/'
 
-AE_TITLE = 'AE_TITLE'
+# Pacs self identification
+AE_TITLE = 'SCHWARZHORN'
+
+# Remote PACS settings
+AE_TITLE = 'MOVESCU'
 AE_CALLED = 'ORTHANC'
 PEER_ADDRESS = '127.0.0.1'
 PEER_PORT = 4242
