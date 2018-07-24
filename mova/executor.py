d24de0114dd0b71531d3d9967282da014e3507b5
Joshy Cyriac <j.cyriac@gmail.com>
2018-07-24 15:00:54 +0200

    should not check

--- mova/executor.py
+++ mova/executor.py
@@ -3,7 +3,6 @@ from subprocess import PIPE
 
 
 def run(cmd):
-    completed = subprocess.run(cmd, stderr=subprocess.STDOUT, shell=False, check=True)
-
+    completed = subprocess.run(cmd, stderr=subprocess.STDOUT, shell=False)
     return completed.returncode, completed.stdout
 
